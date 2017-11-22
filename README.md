d71e855cec10f70eade8c5b01238eb010705c389
liu kai <mylock.kai@gmail.com>
2017-11-22 18:22:54 +0800

    update example and doc

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-## art-qr
+<img src="https://raw.githubusercontent.com/LIU9293/art-qr/master/qr.jpg" align="right" />
+# art-qr
 > Pure JavaScript library to generate beautiful QR code on websites
+
+![travis](https://travis-ci.org/LIU9293/art-qr.svg?branch=master) [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
+### Installation
+```
+yarn add art-qr
+/* or use npm if you prefer*/
+npm install art-qr -S
+```
+
+### Usgae
+```js
+import ArtQR from 'art-qr'
+
+const img = new Image();
+img.crossOrigin = "Anonymous";
+img.src = SharkImg;
+img.onload = () => {
+  new ArtQR().create({
+    text: YOUR_URL_OR_TEXT,
+    size: 300,
+    margin: 10,
+    backgroundImage: img,
+    callback: function (dataUri) {
+      console.log(dataUri)
+    },
+    bindElement: 'qr' // id of <img /> in real dom
+  })
+}
+```
+
+### Credit
+The project is a fork of [Awesome-qr.js](https://github.com/SumiMakito/Awesome-qr.js), and changed
+the build process/code style for esay use.
--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import ArtQR from './qr/bundle.cjs.js';
+import SharkImg from './shark.jpg';
 import './App.css';
 
 class App extends Component {
@@ -9,28 +10,30 @@ class App extends Component {
   }
 
   renderQR = () => {
-    new ArtQR().create({
-      text: 'https://baidu.com',
-      size: 200,
-      margin: 20,
-      colorDark: '#000000',
-      colorLight: '#FFFFFF',
-      backgroundImage: undefined,
-      backgroundDimming: 'rgba(0,0,0,0)',
-      logoImage: undefined,
-      logoScale: 0.2,
-      logoMargin: 0,
-      logoCornerRadius: 8,
-      whiteMargin: true,
-      dotScale: 0.35,
-      autoColor: true,
-      binarize: false,
-      binarizeThreshold: 128,
-      callback: function(dataURI) {
-        console.log(dataURI)
-      },
-      bindElement: 'qrcode'
-    })
+    const img = new Image();
+    img.crossOrigin = "Anonymous";
+    img.src = SharkImg;
+    img.onload = () => {
+      new ArtQR().create({
+        text: 'https://github.com/LIU9293/art-qr',
+        size: 300,
+        margin: 10,
+        colorDark: 'rgba(16, 152, 173, 0.8)',
+        colorLight: '#fff',
+        backgroundImage: img,
+        backgroundDimming: 'rgba(0,0,0,0)',
+        logoImage: undefined,
+        logoScale: 0.2,
+        logoMargin: 0,
+        logoCornerRadius: 8,
+        whiteMargin: true,
+        dotScale: 0.3,
+        autoColor: true,
+        binarize: false,
+        binarizeThreshold: 128,
+        bindElement: 'qrcode'
+      })
+    }
   }
 
   render() {
@@ -40,7 +43,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <img src="" alt="qr" id="qrcode" />
+        <img src="" alt="qr" id="qrcode" className="qr-code" />
       </div>
     );
   }
